e07af43f4fe2109ef6133ba1c6e53b5a4ee4051b
Den <webkrilya@gmail.com>
2020-12-26 16:36:24 +0700

    first

---
+++ ForexTest/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace ForexTest
+{
+    partial class ForexCap
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dg = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dg
+            // 
+            this.dg.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dg.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dg.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dg.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dg.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dg.Location = new System.Drawing.Point(0, 0);
+            this.dg.Name = "dg";
+            this.dg.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dg.RowHeadersVisible = false;
+            this.dg.Size = new System.Drawing.Size(732, 445);
+            this.dg.TabIndex = 0;
+            this.dg.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dg_CellContentClick);
+            this.dg.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dg_MouseDown);
+            this.dg.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dg_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ForexCap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 445);
+            this.Controls.Add(this.dg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ForexCap";
+            this.Text = "ForexCap";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dg;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ForexTest/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace ForexTest
+{
+    public partial class ForexCap : Form
+    {
+
+
+        bool dropper;
+        public ForexCap()
+        {
+            InitializeComponent();
+
+            
+
+
+        }
+
+
+
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+
+        public delegate bool EnumWindowsProc(IntPtr hWnd, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr WindowFromPoint(System.Drawing.Point p);
+
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct COPYDATASTRUCT
+        {
+            public IntPtr dwData; // in C/C++ this is an UINT_PTR, not an UINT
+            public int cbData;
+            public IntPtr lpData;
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 74)
+            {
+                COPYDATASTRUCT data = (COPYDATASTRUCT)m.GetLParam(typeof(COPYDATASTRUCT));
+                byte[] b = new byte[data.cbData-1];
+                Marshal.Copy(data.lpData, b, 0, data.cbData-1);
+                string msg = Encoding.UTF8.GetString(b);
+
+
+
+                Console.WriteLine(msg);
+            }
+            base.WndProc(ref m);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dropper = false;
+
+            this.CenterToScreen();
+            dg.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dg.ColumnHeadersHeight = 24;
+
+
+            dg.Columns.Add("col1", "Ордер");
+            dg.Columns.Add("col2", "Время");
+            dg.Columns.Add("col3", "Тип");
+            dg.Columns.Add("col4", "Объем");
+            dg.Columns.Add("col5", "Символ");
+            dg.Columns.Add("col6", "Цена");
+            dg.Columns.Add("col7", "S / L");
+            dg.Columns.Add("col8", "T / P");
+            dg.Columns.Add("col9", "Цена");
+            dg.Columns.Add("col10", "Комиссия");
+            dg.Columns.Add("col11", "Своп");
+            dg.Columns.Add("col12", "Прибыль");
+
+            
+        }
+
+        private void dg_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+        }
+
+        IntPtr windowHandle = IntPtr.Zero;
+
+        private void dg_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            if (dropper) return;
+
+            dropper = true;
+
+            this.Cursor = Cursors.Arrow;
+
+            
+
+            windowHandle = WindowFromPoint(new Point(Cursor.Position.X, Cursor.Position.Y));
+
+            timer1.Enabled = true;
+
+//            Console.WriteLine(needRect.Left.ToString() + "; " + needRect.Top.ToString());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Rect needRect = new Rect();
+            GetWindowRect(windowHandle, ref needRect);
+
+            this.Left = needRect.Left;
+            this.Top = needRect.Top;
+
+            this.Width = (needRect.Right - needRect.Left);
+            //this.Width = 100;
+            this.Height = needRect.Bottom - needRect.Top;
+
+
+        }
+
+        private void dg_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
